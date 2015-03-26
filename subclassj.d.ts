294f63b8b197d8425d88c8d4da25ebb51e8111d9
SaschaNaz <saschanaz@outlook.com>
2015-03-26 21:38:20 +0900

    Revert "generics"
    This reverts commit 6886627a822334ef871bb1ceb9f4f658e7087e59.

--- subclassj.d.ts
+++ subclassj.d.ts
@@ -1,4 +1,4 @@
 declare module SubclassJ {
     let required: boolean;
-    function getNewThis<T extends Function>(thisArg: T, extending: any, arguments: IArguments | any[]): T;
+    function getNewThis(thisArg: any, extending: any, arguments: IArguments | any[]): any;
 }
